8b8cfe4bfd8802fac2fde1c8abfd8c6e7c54f361
herry88 <herry081288@gmail.com>
2022-01-18 11:15:51 +0700

    inventorynew

---
+++ database/migrations/2022_01_18_040143_create_mitras_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMitrasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mitras', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama')->nullable();
+            $table->text('alamat');
+            $table->string('pic')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mitras');
+    }
+}
