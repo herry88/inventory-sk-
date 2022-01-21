07de560c70a5f5fa8d527a8dec3b8bdeaf40c138
herry88 <herry081288@gmail.com>
2022-01-21 16:05:16 +0700

    addproductinoutpage

---
+++ resources/views/produk-masuk/index.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+
+@section('title')
+    <title>Halaman Produk Masuk </title>
+@endsection
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Produk Masuk </h1>
+        </div>
+    </section>
+@endsection
