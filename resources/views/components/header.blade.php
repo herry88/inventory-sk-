d9288c8a0da12c940eb8a5d7a69d87c6016537a8
Herry Prasetyo <herry081288@gmail.com>
2022-01-23 00:19:23 +0700

    amanlah segini dulu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,9 @@
 <nav class="navbar navbar-expand-lg main-navbar">
     <form class="form-inline mr-auto">
         <ul class="navbar-nav mr-3">
-            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i
-                        class="fas fa-bars"></i></a></li>
-            <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
-                        class="fas fa-search"></i></a></li>
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+            <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a>
+            </li>
         </ul>
         <div class="search-element">
 
@@ -17,14 +16,18 @@
         <li class="dropdown"><a href="#" data-toggle="dropdown"
                 class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                 <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-                <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+                <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
 
                 <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item has-icon text-danger">
-                    <i class="fas fa-sign-out-alt"></i> Logout
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
                 </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
             </div>
         </li>
     </ul>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -6,25 +6,36 @@
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">AU</a>
         </div>
-        <ul class="sidebar-menu">
-            <li class="menu-header">Dashboard</li>
-            <li class="nav-item">
-                <a href="{{ route('dashboard.index') }}" class="nav-link"><i
-                        class="fas fa-fire"></i><span>Dashboard</span></a>
+        @if (Auth::user()->level == 'admin')
+            <ul class="sidebar-menu">
+                <li class="menu-header">Dashboard</li>
+                <li class="nav-item">
+                    <a href="{{ route('dashboard.index') }}" class="nav-link"><i
+                            class="fas fa-fire"></i><span>Dashboard</span></a>
 
-            </li>
-            <li class="menu-header">Main Menu</li>
+                </li>
+                <li class="menu-header">Main Menu</li>
 
-            <li><a class="nav-link" href="{{ route('category.index') }}"><i class="far fa-square"></i>
-                    <span>Category</span></a></li>
-            <li><a class="nav-link" href="{{ route('mitra.index') }}"><i class="far fa-square"></i>
-                    <span>Mitra</span></a></li>
-            <li><a class="nav-link" href="{{ route('product.index') }}"><i class="far fa-square"></i>
-                    <span>Product</span></a></li>
-            <li><a class="nav-link" href="{{ route('product-masuk.index') }}"><i class="far fa-square"></i>
-                    <span>Produk Masuk</span></a></li>
-            <li><a class="nav-link" href="{{ route('product-keluar.index') }}"><i class="far fa-square"></i>
-                    <span>Produk Keluar</span></a></li>
-        </ul>
+                <li><a class="nav-link" href="{{ route('category.index') }}"><i class="far fa-square"></i>
+                        <span>Category</span></a></li>
+                <li><a class="nav-link" href="{{ route('mitra.index') }}"><i class="far fa-square"></i>
+                        <span>Mitra</span></a></li>
+                <li><a class="nav-link" href="{{ route('product.index') }}"><i class="far fa-square"></i>
+                        <span>Product</span></a></li>
+                <li><a class="nav-link" href="{{ route('product-masuk.index') }}"><i class="far fa-square"></i>
+                        <span>Produk Masuk</span></a></li>
+                <li><a class="nav-link" href="{{ route('product-keluar.index') }}"><i
+                            class="far fa-square"></i>
+                        <span>Produk Keluar</span></a></li>
+            </ul>
+        @else
+            <ul class="sidebar-menu">
+                <li><a class="nav-link" href="{{ route('product-masuk.index') }}"><i class="far fa-square"></i>
+                        <span>Produk Masuk</span></a></li>
+                <li><a class="nav-link" href="{{ route('product-keluar.index') }}"><i
+                            class="far fa-square"></i>
+                        <span>Produk Keluar</span></a></li>
+            </ul>
+        @endif
     </aside>
 </div>
