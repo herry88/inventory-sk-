c809b2bd54f905e7cc4178b2975e8b43b54c6dce
herry88 <herry081288@gmail.com>
2022-01-24 13:59:23 +0700

    productkeluarstore done

--- app/Models/ProductOut.php
+++ app/Models/ProductOut.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class ProductOut extends Model
 {
     use HasFactory;
+    protected $fillable = ['no_invoice', 'product_id', 'mitra_id', 'stock', 'keterangan'];
+
+
+    /**
+     * Get the user that owns the ProductIn
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function mitraname()
+    {
+        return $this->belongsTo(Mitra::class, 'mitra_id');
+    }
+
+    /**
+     * Get the user that owns the ProductIn
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function productname()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
---
+++ resources/views/produk-keluar/form.blade.php
@@ -0,0 +1,60 @@
+@extends('master')
+
+@section('title')
+    <title>Form Produk Keluar</title>
+@endsection
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Product Out</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item"><a href="#">Forms</a></div>
+                <div class="breadcrumb-item">Editor</div>
+            </div>
+        </div>
+        <div class="section-body">
+            <div class="section-title">Product Out</div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Product Out Create</h4>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('product-keluar.store') }}" method="post">
+                                @csrf
+                                @method('POST')
+                                <div class="form-group">
+                                    <label for="">No Invoice</label>
+                                    <input type="text" name="no_invoice" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Product</label>
+                                    {!! Form::select('product_id', $product, null, ['class' => 'form-control select', 'placeholder' => '-- Choose Product --', 'id' => 'product_id', 'required']) !!}
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Mitra</label>
+                                    {!! Form::select('mitra_id', $mitra, null, ['class' => 'form-control select', 'placeholder' => '-- Choose Product --', 'id' => 'mitra_id', 'required']) !!}
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Stock</label>
+                                    <input type="text" class="form-control" placeholder="stock" name="stock">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Keterangan</label>
+                                    <input type="text" name="keterangan" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                    <a href="" class="btn btn-default pull-left">Cancel</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
